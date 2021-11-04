e560fc5ed2a52845f43726eb63bbe30c1fb3e89d
RussellWaring <russell.waring@dcmail.ca>
2021-11-04 14:53:02 -0400

    Added user input

--- src/Program.java
+++ src/Program.java
@@ -1,10 +1,11 @@
 /**
- * @author      Russell Waring
- * @studentID   100528426
- * @date        November 4, 2021
- * @desc        These files have been created following along in-class of week 8.
+ * @author          Russell Waring, 100528426
+ * @date            November 4, 2021
+ * @description     These files have been created following along in-class of week 8.
  *
  */
+
+import java.util.Scanner; // Import the Scanner class
 
 public class Program
 {
@@ -18,6 +19,10 @@ public class Program
         return value * value;
     }
 
+    /**
+     * The entry point for the program
+     * @param args an array of command line parameters
+     */
     public static void main(String[] args)
     {
         System.out.println("Hello, World!");
@@ -25,9 +30,19 @@ public class Program
         float valueToSquare = 5;
         float squaredValue = square(valueToSquare);
 
+        // example of output
         System.out.println("The Squared Value is: " + squaredValue);
 
-        Person person = new Person("Tom", 30);
+        // Step 1. for input we need to create a Scanner object
+        Scanner inputObject = new Scanner(System.in);
+        System.out.print("Enter your name: "); // print is like a newline
+        // Step 2. use the Scanner object to get the "next" line of data in this case a String value
+        String name = inputObject.nextLine();
+
+        System.out.print("Enter your age: ");
+        int age = inputObject.nextInt(); // converts from string, to input, to int
+
+        Person person = new Person(name, age); // using name value
         person.SaysHello();
     }
 }
